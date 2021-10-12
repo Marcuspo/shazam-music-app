14f12a0a090e1fa93c5f24c4d89dc89acc83365c
Marcus Pontes <marcusporama@gmail.com>
2021-10-12 08:12:05 -0300

    finalizado a tela inicial e criação de arquivos como rock, pop, lofi e classica para renderização dos novos componentes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,48 @@
+import { Button, Input } from "antd"
+
+import { useState } from "react"
+import api from "./api/api"
+
+import { NavBar, TextNav, ContainerInput } from "./Style"
+
 function App() {
+  const [data, setData] = useState([])
+  const [search, setSearch] = useState("")
+
+  const openApi = "864a189243203c0746b9fff8f966f747"
+
+  async function changeSearch(event) {
+    setSearch(event.target.value)
+    console.log(search)
+  }
+
+  async function fetchData() {
+    const response = await api.get(
+      `/weather?q=${search}&appid=${openApi}&units=metric`
+    )
+    console.log(response)
+    setData(response.data)
+  }
+
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <NavBar>
+        <TextNav>Músicas!!!</TextNav>
+      </NavBar>
+
+      <ContainerInput>
+        <form>
+          <Input
+            placeholder="Digite sua cidade"
+            value={search}
+            onChange={changeSearch}
+            style={{ fontSize: 20 }}
+          />
+          <Button style={{ marginTop: 15 }} type="primary" onClick={fetchData}>
+            Post
+          </Button>
+        </form>
+      </ContainerInput>
     </div>
   )
 }
---
+++ src/components/Style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+
+export const NavBar = styled.h1`
+  width: 100%;
+  height: 91px;
+  background-color: #6324c6;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+    rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+`
+
+export const TextNav = styled.span`
+  font-family: "Mulish", sans-serif;
+  color: #fff;
+  text-transform: uppercase;
+  font-style: normal;
+  font-size: 36px;
+  line-height: 45px;
+`
+export const ContainerInput = styled.div`
+  width: 90%;
+
+  justify-content: center;
+  align-items: center;
+  padding-left: 10%;
+  padding-top: 5%;
+  color: black;
+`
